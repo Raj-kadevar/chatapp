4810affa15f29394e0698bbd0608551422f18444
Raj kadevar <raj.kadevar@trootech.com>
2023-12-12 15:47:19 +0530

    update readme file

--- chatapp/chatapp/settings/test_settings.py
+++ chatapp/chatapp/settings/test_settings.py
@@ -1,9 +1,13 @@
 from .base import *
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        'default': {
+        'ENGINE': env.str('ENGINE'),
+        'NAME': env.str('NAME'),
+        'USER': env.str('USER_NAME'),
+        'PASSWORD': env.str('PASSWORD'),
+        'HOST': env.str('HOST'),
+        'PORT': env.str('PORT'),
     }
 }
 
